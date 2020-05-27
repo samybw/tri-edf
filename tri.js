b70e117b7d0471db8a631c84baad8c6a562e225c
samy <samybn@yahoo.com>
2020-05-27 10:40:37 +0200

    ajour de la boucle et de la variable resultat

--- tri.js
+++ tri.js
@@ -1,7 +1,16 @@
 var nombre = process.argv.slice(2);
-var nomb = nombre.map(Number)
+var nomb = nombre.map(Number);
+var resultat = []
+var i = 0;
+while (i < nomb.length) {
+  if (nomb[i] > nomb[i+1]) {
+    resultat += nomb.unshift(i)
+  }else{
+    resultat += nomb.push(i)
+  }
+  i++
+}
 
-
-console.log(nomb);
+console.log(resultat);
 
 
