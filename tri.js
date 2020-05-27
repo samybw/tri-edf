53278475b33fe0278aa88d99f62c6435cb823e24
samy <samybn@yahoo.com>
2020-05-27 12:05:50 +0200

    je change resultat en temp j'ai aussi rajoute une autre boucle pour bloqué la boucle infinie

--- tri.js
+++ tri.js
@@ -1,20 +1,23 @@
 var nombre = process.argv.slice(2);
 var nomb = nombre.map(Number);
-var resultat ;
+var temp = [];
+var cond = true 
+while (cond) {
+  cond = false;
+
 var i = 0;
 while (i < nomb.length - 1) {
   if (nomb[i] < nomb[i + 1 ]) {
-    resultat = nomb[i]
+    temp = nomb[i]
     nomb[i] = nomb[i + 1]
-    nomb[i + 1] = resultat
+    nomb[i + 1] = temp
+    cond = false;
 
-
-  }else{
-    
   }
   i++
+ }
 }
 
-console.log(resultat);
+console.log(nomb);
 
 
